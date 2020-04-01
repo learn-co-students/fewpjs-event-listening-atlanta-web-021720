c2f2c434879fbcc7f987bc9e10f654176d3bb430
SpencerBlum <spencerblum@gmail.com>
2020-04-01 18:48:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
+const input = document.getElementById('input');
+
+input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
+
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+      });
+
 }
